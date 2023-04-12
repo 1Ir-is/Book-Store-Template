7b996783a3a3b0803e29c48326ad30a070dc5a17
1Ir-is <mhuynk1005@gmail.com>
2023-04-12 19:05:19 +0700

    Design arrivals books section

--- js/app.js
+++ js/app.js
@@ -64,3 +64,24 @@ var swiper = new Swiper(".featured-slider", {
         },
     }
 });
+
+var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+});
