0d89af550054f85797bfb8a6b5d3d12ba34c6b5e
1Ir-is <mhuynk1005@gmail.com>
2023-04-12 18:07:00 +0700

    Design featured book

--- js/app.js
+++ js/app.js
@@ -36,3 +36,31 @@ window.onload = () =>{
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
+
+var swiper = new Swiper(".featured-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        450:{
+            slidesPerView: 2,
+        },
+        768:{
+            slidesPerView: 3,
+        },
+        1024:{
+            slidesPerView: 4,
+        },
+    }
+});
