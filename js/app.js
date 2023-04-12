f406ecf69b1c668e8fe23be5b4e5af79361ddad9
1Ir-is <mhuynk1005@gmail.com>
2023-04-12 14:17:59 +0700

    design login form

--- js/app.js
+++ js/app.js
@@ -4,6 +4,15 @@ document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
